97d46a6b1610d64f2e367b8dbaa6e1a082f7abc0
Oskar Bergman <ocke_bergman@hotmail.com>
2023-11-09 11:17:15 +0100

    added animations

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const hamburgerOpen = document.querySelector('.hamburger__button--open');
 const hamburgerClose = document.querySelector('.hamburger__button--close');
-const hamburgerNavCont = document.querySelector('.hamburger__nav__container');
+const hamburgerNavCont = document.querySelector('.hamburger__container');
 const hamburgerNavList = document.querySelector('.hamburger__nav');
 
 hamburgerOpen.addEventListener('click', () => {
