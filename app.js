31797d6244ab96c71780f1f74ac39644634737f1
Oskar Bergman <ocke_bergman@hotmail.com>
2023-11-17 11:28:09 +0100

    added fetch api

--- app.js
+++ app.js
@@ -24,3 +24,13 @@ const filterOpen = document.querySelector(".title__filterBtn");
 filterOpen.addEventListener("click", function () {
   console.log("Funkar det?");
 });
+
+/* FETCH API */
+
+async function fetchApi() {
+
+  const url = 'https://lernia-sjj-assignments.vercel.app/api/challenges';
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
